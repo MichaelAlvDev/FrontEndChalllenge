3c6d10cfc4049544fe5e7653a08bdde92af7a2a4
Michael Alvarez <andrikolalv@gmail.com>
2019-11-19 19:54:26 -0400

    Agregado toggle open/close a las listas de categorias

--- src/components/menu_categories/menu_categories.jsx
+++ src/components/menu_categories/menu_categories.jsx
@@ -1,17 +1,28 @@
-import React, { Component,fragment } from "react";
+import React, { Component, Fragment } from "react";
+import "./menu_categories.scss";
 const APICategories = "/data/categories.json";
 
+// alterna las clses "open/closed" para desplegar las listas
+function toggle_list(e){
+  e.preventDefault();
+  let listTarget= e.target.parentElement;
+  
+  if (listTarget.classList.contains('open')){
+    listTarget.classList.replace('open', 'closed');
+  }else if(listTarget.classList.contains('closed')){
+    listTarget.classList.replace('closed', 'open');
+  }
+}
 const buildMenu = function buildMenu(items, nl = 0) {
   return (
-    <ul>
+    <ul className="list-group mt-3 ">
       {items.map(item => (
-        <li key={item.id} className=""> 
-          <a href="/#">
+        <li key={item.id} className="list-group-item closed" nestingLvl={nl} >
+          <a href="/#" className="text-white" onClick={toggle_list}>
             {item.name}
-            <i class="fas fa-plus-circle"></i>
           </a>
           {item.sublevels && item.sublevels.length > 0
-            ? buildMenu(item.sublevels)
+            ? buildMenu(item.sublevels, nl + 1)
             : null}
         </li>
       ))}
@@ -43,12 +54,12 @@ class menuCategories extends Component {
   render() {
     const categories = this.state.dataCategories;
     return (
-      <ul className="list-group mt-3">
+      <Fragment>
         {buildMenu(categories)}
         {/* {categories.map(categorie=>(
                 <a href="/#" class="list-group-item list-group-item-action">{categorie.name}<i class="fas fa-plus-circle"></i></a>
             ))} */}
-      </ul>
+      </Fragment>
     );
   }
 }
