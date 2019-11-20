312a1bc99d76c8c3cba605e9912a6d4d10738cf8
Michael Alvarez <andrikolalv@gmail.com>
2019-11-19 21:43:35 -0400

    Migrar fetch a componente principal "App" para poder heredar props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './components/header/header';
 import Sidebar from './components/sidebar/sidebar';
 import MainProductsGrid from './components/main_products_grid/main_products_grid';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Sidebar/>
-      <MainProductsGrid/>
-    </div>
-  );
+const APIProducts = "/data/products.json";
+
+class App extends Component {
+ 
+  state = {
+    dataProducts: []
+  };
+  // llamada a funcion de Fetch para obtener datos de productos
+  componentDidMount() {
+    this.obtenerProductos();
+  }
+  obtenerProductos() {
+    //Conexion al fetch
+    fetch(APIProducts)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          dataProducts: data.products
+        });
+      })
+      .catch(function(error) {
+        console.log(`error conectando a productos ${error}`);
+      });
+  }
+  render() { 
+    return ( <div className="App">
+       <Header/>
+       <Sidebar/>
+       <MainProductsGrid
+        dataProducts = {this.state.dataProducts}
+       />
+     </div> );
+  }
 }
 
 export default App;
--- src/components/main_products_grid/main_products_grid.jsx
+++ src/components/main_products_grid/main_products_grid.jsx
@@ -1,34 +1,14 @@
 import React, { Component } from "react";
 import "./main_products_grid.scss";
 import Product from "../product/product.jsx";
-const APIProducts = "/data/products.json";
 
 class main_products_grid extends Component {
-  state = {
-    dataProducts: []
-  };
-  // llamada a funcion de Fetch para obtener datos de productos
-  componentDidMount() {
-    this.obtenerProductos();
-  }
-  obtenerProductos() {
-    //Conexion al fetch
-    fetch(APIProducts)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          dataProducts: data.products
-        });
-      })
-      .catch(function(error) {
-        console.log(`error conectando a productos ${error}`);
-      });
-  }
+  state = {  }
   render() {
     //Data original
-    const dataProducts = this.state.dataProducts;
+    const dataProducts = this.props.dataProducts;
     //Data luego de filtrar
-    let dataProductsFiltered = dataProducts;
+    let dataProductsFiltered = dataProducts.filter(dataProducts=> dataProducts.sublevel_id===3);
     return (
       <div className="jumbotron col-md-10 Grid-container">
         {dataProductsFiltered.map(product => (
